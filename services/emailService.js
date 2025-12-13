d34f0a233738508220854ea1d33de59d762b342f
rafaelrar1986-del <rafaelrar1986@gmail.com>
2025-12-13 10:57:12 -0300

    Update emailService.js

--- services/emailService.js
+++ services/emailService.js
@@ -3,7 +3,7 @@ const nodemailer = require('nodemailer');
 const transporter = nodemailer.createTransport({
   host: process.env.SMTP_HOST,
   port: Number(process.env.SMTP_PORT),
-  secure: false, // 587 = false
+  secure: true, // 587 = false
   auth: {
     user: process.env.SMTP_USER,
     pass: process.env.SMTP_PASS
