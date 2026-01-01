74293b23c2aa29563eaedbe118252843089b1259
rafaelrar1986-del <rafaelrar1986@gmail.com>
2026-01-01 09:33:52 -0300

    Update NewsMessage.js

--- models/NewsMessage.js
+++ models/NewsMessage.js
@@ -1,5 +1,21 @@
 const mongoose = require('mongoose');
 
+const NewsReactionSchema = new mongoose.Schema(
+  {
+    emoji: {
+      type: String,
+      required: true
+    },
+    users: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User'
+      }
+    ]
+  },
+  { _id: false }
+);
+
 const NewsMessageSchema = new mongoose.Schema(
   {
     user: {
@@ -7,11 +23,25 @@ const NewsMessageSchema = new mongoose.Schema(
       ref: 'User',
       required: true
     },
+
     text: {
       type: String,
       required: true,
       maxlength: 80,
       trim: true
+    },
+
+    /* =========================
+       😀 REAÇÕES
+       Ex:
+       reactions: [
+         { emoji: "😂", users: [userId1, userId2] },
+         { emoji: "🔥", users: [userId3] }
+       ]
+    ========================= */
+    reactions: {
+      type: [NewsReactionSchema],
+      default: []
     }
   },
   {
