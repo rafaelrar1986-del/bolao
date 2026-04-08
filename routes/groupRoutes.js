d3b66e6ffc60a1b873e28f2e79ede10a67332503
rafaelrar1986-del <rafaelrar1986@gmail.com>
2026-04-08 16:05:28 -0300

    Delete routes directory

--- routes/groupRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { getGroupStandings } = require('../controllers/groupController');
-
-// Rota: GET /api/groups/standings
-router.get('/standings', getGroupStandings);
-
-module.exports = router;
--- routes/rankingRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const rankingController = require('../controllers/rankingController');
-
-// Agora o endpoint é /api/bets/partialrank
-router.get('/partialrank', rankingController.getRanking);
-
-module.exports = router;
