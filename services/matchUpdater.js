a3cf4ce2817d77496ffc7d9a248a994c8bd83034
rafaelrar1986-del <rafaelrar1986@gmail.com>
2026-04-10 11:06:56 -0300

    Update matchUpdater.js

--- services/matchUpdater.js
+++ services/matchUpdater.js
@@ -1,4 +1,4 @@
-]const axios = require('axios');
+const axios = require('axios');
 const Match = require('../models/Match'); 
 const { recalculateAllPoints } = require('./pointsService');
 const { trySaveDailyPoints } = require('./dailyHistoryService');
