bf4218fd8743ba8303897fbbe35b98e73f6e64b0
rafaelrar1986-del <rafaelrar1986@gmail.com>
2026-09-01 11:20:28 -0300

    Delete tests directory

--- tests/group-qualification-rules-ui.test.js
+++
@@ -1,7 +0,0 @@
-
-const fs=require('fs'), path=require('path');
-const s=fs.readFileSync(path.join(__dirname,'..','js','admin.js'),'utf8');
-for(const token of ['positionCorrect','positionIncorrect','teamQualified','teamNotQualified','groupQualificationRules','btn-add-group-qualification-rule','gqr-rule-points']){
- if(!s.includes(token)) throw new Error('Token ausente: '+token);
-}
-console.log('group-qualification-rules-ui.test.js: OK');
